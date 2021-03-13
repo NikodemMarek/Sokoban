2ad5d4506e0ed74c5b4ce17f029964de94d04d13
Nikodem <62289991+NikodemMarek@users.noreply.github.com>
2021-03-13 23:06:36 +0100

    feat: css separate from html file

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ let context = canvas.getContext('2d');
 
 let game = new Game(context);
 game.start();
+
+let resetLevel = function() {
+    game = new Game(context);
+    game.start();
+}
+
+document.getElementById('resetLevel').addEventListener('click', resetLevel);
