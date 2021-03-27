cd78b67de1310328a2499db87fc668fa9cc1edeb
Nikodem <62289991+NikodemMarek@users.noreply.github.com>
2021-03-27 22:45:24 +0100

    feat: documentation for game directory

--- src/game/scoreCounter.js
+++ src/game/scoreCounter.js
@@ -1,3 +1,7 @@
+/**
+ * @module scoreCounter
+ */
+
 export function calculateScore(scoreMultiplier, movesMade, movesUndone) {
     let score =  1000 - (movesMade + movesUndone * movesUndone) * scoreMultiplier;
     return score >= 0 ? score: 0;
