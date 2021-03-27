fa48d6a14846c65dcf7fb5fe392bbd7bbb28991d
Nikodem <62289991+NikodemMarek@users.noreply.github.com>
2021-03-27 23:25:34 +0100

    feat: documentation for objects directory

--- src/objects/box.js
+++ src/objects/box.js
@@ -1,14 +1,37 @@
+/**
+ * @module box
+ */
+
 import { BOARD_DIMENSIONS } from '/src/constants.js'
 
+/**
+ * Przechowuje informacje o pudełku i pozwala na wykonywanie operacji na nim.
+ */
 export default class Box {
+    /**
+     * Przyjmuje i zapisuje do zmiennych informacje o pudełku.
+     * @param {index:Position} position - Pozycja w której znajduje się pudełko
+     * @param {boolean} inPlace - Pudełko jest / nie jest na celu
+     */
     constructor(position, inPlace = false) {
         this.position = position
         this.inPlace = inPlace
     }
 };
 
+/**
+ * Rysuje pudełko na {@link index:context}.
+ * @param {Box} box - {@link Box}
+ * @param {CanvasImage} canvasImage - {@link canvasImage:CanvasImage}
+ */
 export function draw(box, canvasImage) { canvasImage.drawBox(box.position) }
 
+/**
+ * Porusza pudełko o ilość jednostek przekazanych do funkcji.
+ * @param {Box} box - {@link Box}
+ * @param {index:Position} move - Ilość jednostek o które zostanie przesunięte pudełko
+ * @returns {boolean} Pudełko zostało / nie zostało (na drodze pudełka jest przeszkoda) przesunięte
+ */
 export function move(box, { x = 0, y = 0 } = {}) {
     if(
         box.position.x + x >= 0 &&
--- src/objects/worker.js
+++ src/objects/worker.js
@@ -1,15 +1,34 @@
+/**
+ * @module worker
+ */
+
 import { BOARD_DIMENSIONS } from '/src/constants.js'
 
+/**
+ * Przechowuje informacje o magazynierze i pozwala na wykonywanie na nim operacji.
+ */
 export default class Worker {
+    /**
+     * Przyjmuje i zapisuje do zmiennej początkową pozycje magazyniera.
+     * @param {index:Position} position - Pozycja początkowa magazyniera
+     */
     constructor(position) {
         this.position = position;
     }
 };
 
-// draw worker
+/**
+ * Rysuje magazyniera na planszy.
+ * @param {Worker} worker - {@link worker:Worker}
+ * @param {canvasImage:CanvasImage} canvasImage - {@link canvasImage:CanvasImage}
+ */
 export function draw(worker, canvasImage) { canvasImage.drawWorker(worker.position) }
 
-// change worker by x and y
+/**
+ * Sprawdza czy magazynier może poruszyć się o daną liczbę jednostek i jeśli może, porusza go.
+ * @param {Worker} worker - {@link Worker}
+ * @param {index:Position} direction - Liczba jednostek o które ma zostać poruszony magazynier
+ */
 export function move(worker, { x = 0, y = 0 } = {}) {
     if(worker.position.x + x >= 0 && worker.position.x + x < BOARD_DIMENSIONS.x) worker.position.x += x;
     if(worker.position.y + y >= 0 && worker.position.y + y < BOARD_DIMENSIONS.y) worker.position.y += y;
